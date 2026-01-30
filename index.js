392a8a1368cfdfbd5740499786d76f71796b6513
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-30 11:06:30 +0530

    new contents added

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 (async function () {
     
-    
+const data=await fetch("./data.json");
+// console.log(data);
+const res=await data.json();
+// console.log(res);
+
+let employee=res;
+
+let selectedEmployeeId = employee[0].id;
+let selectedEmployee = employee[0];
+
+const employeeList = document.querySelector(".employees__names--list")
+const employeeInfo = document.querySelector(".employees__single--list")
+
+
 })();
